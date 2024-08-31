7c1d737686720043bdf3a2d5e3a4a9c49d46df4a
teegee-123 <u10132806@gmail.com>
2024-08-31 22:01:52 +0200

    added API

---
+++ api.py
@@ -0,0 +1,20 @@
+import os
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from forwarder import main
+load_dotenv()
+code_file = os.environ.get("CODE_FILE")
+
+app = FastAPI()
+
+
+@app.get("/code/{code}")
+async def set_code(code: int):
+     print(code_file)
+     f = open(code_file, "w")
+     f.write(str(code))
+     return code
+
+@app.get("/run")
+async def set_code():
+     await main()
--- forwarder.py
+++ forwarder.py
@@ -3,14 +3,15 @@ import json
 from telethon.sync import TelegramClient, events
 from dotenv import load_dotenv, dotenv_values 
 from telethon.tl.functions.channels import CreateChannelRequest, InviteToChannelRequest
-
+import asyncio
+import time
 load_dotenv()
 
 phone = os.environ.get("PHONE")
 session = os.environ.get("SESSION")
 api_id = os.environ.get("API_ID") 
 api_hash = os.environ.get("API_HASH")
-
+code_file = os.environ.get("CODE_FILE")
 trade_bot = os.environ.get("TRADEBOTNAME")
 buy_signals = int(os.environ.get("BUYSIGNALSGROUP"))
 
@@ -20,7 +21,7 @@ feed_users = json.loads(os.environ.get("FEED_USERS")) # add safe analyzer and th
 print(feeds)
 
 
-def getMessageId(message):
+def getIdFromMessage(message):
    if(hasattr(message.peer_id, 'channel_id')):            
       return message.peer_id.channel_id 
    if(hasattr(message.peer_id, 'user_id')):
@@ -28,51 +29,52 @@ def getMessageId(message):
    if(hasattr(message.peer_id, 'chat_id')):
       return message.peer_id.chat_id
 
-async def getChats(client): 
-   async for dialog in client.iter_dialogs():
-      with open("chats3.txt", "a", encoding="utf-8") as myfile:
-         id = getMessageId(dialog.message)
-         print(f'{dialog.name}:{id}\n')
-         myfile.write(f'{dialog.name}:{id}\n')
-
-
-def create_feed(client , feed_name):      
-   createdGroup = client(CreateChannelRequest(f'Feed {feed_name}', f'forwards from {feed_name}' ,megagroup=True))
+async def create_feed(client , feed_name):      
+   createdGroup = await client(CreateChannelRequest(f'Feed {feed_name}', f'forwards from {feed_name}' ,megagroup=True))
+   print(createdGroup)
    newChannelID = createdGroup.__dict__["chats"][0].__dict__["id"]     
-   client(InviteToChannelRequest(channel=newChannelID, users=feed_users))
+   await client(InviteToChannelRequest(channel=newChannelID, users=feed_users))
 
-   users = client.get_participants(newChannelID)
+   users = await client.get_participants(newChannelID)
    client.edit_admin(add_admins=True, entity=newChannelID, user = users[1], post_messages = True, edit_messages = True)
    client.edit_admin(add_admins=True, entity=newChannelID, user = users[2], post_messages = True, edit_messages = True)
    return newChannelID
 
 # creates a group with scraper bot and safe bot as participants and for
 # also listens for messages on buy signals and forwards to trade bot
-def create_feeds():
-   channels = []
-   with TelegramClient(session, api_id, api_hash) as client:    
-      for feed in feeds:
-         feed_name = feed["name"]
-         source_id = feed["id"]
-         id = create_feed(client, feed_name)
-         if(not feed["channel_id"]):
-            feed["channel_id"] = id
+async def create_feeds(client):   
+   print("CONNECTED")
+   for feed in feeds:
+      feed_name = feed["name"]
+      source_id = feed["id"]
+      id = await create_feed(client, feed_name)
+      feed["channel_id"] = id
+
+   sources = list(map(lambda x: x['id'], feeds))
+   @client.on(events.NewMessage(chats=sources))
+   async def handler(event):
+      source_id = getIdFromMessage(event.message)
+      destination_id = list(filter(lambda x: x['id'] == source_id, feeds))[0]['channel_id']
+      await client.send_message(destination_id, event.message)
       
-      sources = list(map(lambda x: x['id'], feeds))
-      @client.on(events.NewMessage(chats=sources))
-      async def handler(event):
-         source_id = getMessageId(event.message)
-         destination_id = list(filter(lambda x: x['id'] == source_id, feeds))[0]['channel_id']
-         await client.send_message(destination_id, event.message)
-      
-      @client.on(events.NewMessage(chats=[buy_signals]))
-      async def handler(event):         
-         await client.send_message(trade_bot, event.message)
+   @client.on(events.NewMessage(chats=[buy_signals]))
+   async def handler(event):         
+      await client.send_message(trade_bot, event.message)
 
 
-      client.run_until_disconnected()
+def getCodeFromFile(): 
+   time.sleep(15)
+   with open(code_file, "r", encoding="utf-8") as myfile:
+      code = myfile.read()
+      print(code)
+      return code
 
-create_feeds()
+async def main():
+   client = TelegramClient(session, api_id, api_hash)
+   await client.start(phone=phone, code_callback=getCodeFromFile)
+   await create_feeds(client)
+   await client.run_until_disconnected()
 
-
+if __name__ == "__main__":
+    asyncio.run(main())
 
