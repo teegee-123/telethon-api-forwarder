246e8c05c86b6b4375567db97c330192666a42bf
teegee-123 <u10132806@gmail.com>
2024-09-03 11:00:37 +0200

    fixes

--- forwarder.py
+++ forwarder.py
@@ -23,7 +23,7 @@ print(feeds)
 print(report_groups)
 
 
-def get_group_users(group_name, groups):
+def get_group_users_to_add(group_name, groups):
    return list(filter(lambda x: x["name"]==group_name ,groups))[0]["users"]
 
 def getSenderIdFromMessage(message):
@@ -40,89 +40,85 @@ def format_id(id):
       id = str(id)[3:]
    return int(id)
 
-async def get_group_id(client, feed_name, prefix = None):
-   print("looking for ")
-   print(f'{prefix} {feed_name}' if prefix is not None else feed_name)
+async def find_group_by_name(client, group_name):   
+   print(f'looking for {group_name}')   
    async for dialog in client.iter_dialogs():
-      if(dialog.title is not None and dialog.title == f'{prefix} {feed_name}' if prefix is not None else feed_name):
+      if(dialog.title is not None and dialog.title == group_name):
+         print(f'Found Group {group_name}')
          return format_id(dialog.id)
-   return 0
+   return None
+
+
+async def create_group(client, group_name, group_users, prefix = None, description = "no description"):
+   group_full_name = group_name if prefix is None else f'{prefix} {group_name}'
+   group_id = await find_group_by_name(client, group_full_name)
+   if(group_id is None):      
+      createdGroup = await client(CreateChannelRequest(f'{prefix} {group_name}', description, megagroup=True))
+      group_id = createdGroup.__dict__["chats"][0].__dict__["id"]
+      print(f'created group {group_full_name} with id {group_id}')
+      await client(InviteToChannelRequest(channel=group_id, users=group_users))
+      await give_users_admin_rights(client, group_id)
+   else:
+      print(f'used exiting group {group_full_name} with id {group_id}')
+   
+   return group_id
+
+
+async def give_users_admin_rights(client, channel_id):
+   users = await client.get_participants(channel_id)
+   for u in users:
+      if(u.bot):
+         print(f'adding rights for {u.username}' )
+         await apply_admin_to_user(client, u, channel_id)
+
 
 async def apply_admin_to_user(client, user, channelId):
    await client.edit_admin(add_admins=True, entity=channelId, user = user, post_messages = True, edit_messages = True)
 
 async def create_feed(client , feed_name, source_id): 
-   newChannelID = await get_group_id(client, feed_name, 'Feed')
-   if(newChannelID == 0):
-      createdGroup = await client(CreateChannelRequest(f'Feed {feed_name}', f'forwards from {feed_name} {source_id}' ,megagroup=True))
-      newChannelID = createdGroup.__dict__["chats"][0].__dict__["id"]
-      print(f'created new group {newChannelID}')
-      await client(InviteToChannelRequest(channel=newChannelID, users=get_group_users(feed_name, feeds)))
-   else:
-      print(f'using existing group {newChannelID}')
-   users = await client.get_participants(newChannelID)
-   for u in users:
-      if(u.bot):
-         print(f'adding rights for {u.username}' )
-         await apply_admin_to_user(client, u, newChannelID)
-   return newChannelID
+   group_users = get_group_users_to_add(feed_name, feeds)
+   group_id = await create_group(client, feed_name, group_users, 'Feed', f'forwards from {source_id}')   
+   return group_id
 
-async def create_report_group(client, report_group_name):
-   print('create_report_group')
-   print(report_group_name)
-   newChannelID = await get_group_id(client, report_group_name, "Report")
-   print("newChannelID REPORT ")
-   print(newChannelID)
-   if(newChannelID == 0):
-      createdGroup = await client(CreateChannelRequest(f'Report {report_group_name}', f'reports for {report_group_name}' ,megagroup=True))
-      newChannelID = createdGroup.__dict__["chats"][0].__dict__["id"]
-      print(f'created new report group {newChannelID}')
-      await client(InviteToChannelRequest(channel=newChannelID, users=get_group_users(report_group_name, report_groups)))
-   else:
-      print(f"using existing report group {newChannelID}")
-   
-   users = await client.get_participants(newChannelID)
-   for u in users:
-      if(u.bot):
-         print(f'adding rights for {u.username}' )
-         await apply_admin_to_user(client, u, newChannelID)
-   return newChannelID
+async def create_report_group(client, report_group_name, group_description):
+   group_users = get_group_users_to_add(report_group_name, report_groups)
+   group_id = await create_group(client, report_group_name, group_users, "Report", group_description)
+   return group_id
 
 async def find_report_destination(message_from_id):
    # flat map all report feeds
    report_feeds = list(itertools.chain.from_iterable(map(lambda x: x["feeds"], report_groups)))
-   print("report groups")
-   print(report_groups)
-   print("report feeds")
-   print(report_feeds)
-   print("message from id")
-   print(message_from_id)
    destination_report_id = list(filter(lambda x: x["channel_id"]==message_from_id, report_feeds))
    if(len(destination_report_id) > 0):
       return destination_report_id[0]["report_channel_id"]
    return None
-# creates a group with scraper bot and safe bot as participants and for
-# also listens for messages on buy signals and forwards to trade bot
-async def create_groups(client):   
-   print("CONNECTED")
+
+async def create_feed_groups(client):
    for feed in feeds:
       feed_name = feed["name"]
       source_id = feed["id"]
       feed["channel_id"] = await create_feed(client, feed_name, source_id)
-
+   
+async def create_report_groups(client):
    for report_group in report_groups:
       group_name = report_group["name"]      
-      report_group["report_channel_id"] = await create_report_group(client, group_name)
+      report_group["report_channel_id"] = await create_report_group(client, group_name, 'reports for addChannelNames')
       for feed in report_group["feeds"]:
          feed["report_channel_id"] = report_group["report_channel_id"]
-         feed["channel_id"] = await get_group_id(client, feed["name"], None)
-         if(feed["channel_id"] == 0):
-            print(f'Check your configs Could not find a report group for {feed["name"]}')
+         feed["channel_id"] = await find_group_by_name(client, feed["name"])
+         # if(feed["channel_id"] == 0):
+         #    print(f'Check your configs Could not find a report group for {feed["name"]}')
 
+# creates a group with scraper bot and safe bot as participants and for
+# also listens for messages on buy signals and forwards to trade bot
+async def create_groups(client):   
+   print("CONNECTED")
+   await create_feed_groups(client)
+   await create_report_groups(client)
 
    feed_sources = list(map(lambda x: x['id'], feeds))
-   destinations = list(map(lambda x: x['channel_id'], feeds))
 
+   print("Listening...")
    #forward from sources to feed groups
    @client.on(events.NewMessage(chats=feed_sources))
    async def handler(event):      
@@ -135,9 +131,8 @@ async def create_groups(client):
    #forward from feed groups to report group
    @client.on(events.NewMessage(chats=feed_groups))
    async def handler(event):
-      print("feed analyzer response")
-      print(str(event.message))
       if("SafeAnalyzer" in str(event.message)):
+         print("feed analyzer response")         
          channelId = getSenderIdFromMessage(event.message)
          destination_report_id = await find_report_destination(channelId)
          if(destination_report_id is not None):
@@ -145,9 +140,10 @@ async def create_groups(client):
             await client.send_message(destination_report_id, event.message)
          else: 
             print(f'could not find a destination for {channelId}')
+            return 
       
 
-
+   # forward from buy signals group to trade bot
    @client.on(events.NewMessage(chats=[buy_signals]))
    async def handler(event):         
       print("Forward to trade bot")
@@ -159,8 +155,7 @@ def getCodeFromFile(delay = 15):
    while(code == ''):
       with open(code_file, "r", encoding="utf-8") as myfile:
          code = myfile.read()
-         print(code)
-         return code
+   print(code)
    return code
    # if(delay != 0): 
    #    time.sleep(delay)
